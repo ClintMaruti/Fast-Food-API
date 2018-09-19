60d64b675ab7a2de216f3bfcddfce6b38abdf352
Clint <cmaruti93@gmail.com>
2018-09-20 01:41:07 +0300

    [Chore #160639968] Tests for GET endpoints

--- tests/test_oders.py
+++ tests/test_oders.py
@@ -5,9 +5,19 @@ from app import create_app
 app = create_app(config_name="TESTING")
 client = app.test_client()
 
+################# Tests for GET endpoints ##################
+
 def test_all_orders():
     """
         Test for GET all orders
     """
     res = client.get('api/v1/orders')
     assert res.status_code == 200
+
+def test_get_specific_order():
+    """
+        Test for GET specific orders
+    """
+    res = client.get('api/v1/orders/0')
+    assert res.status_code == 200
+    
