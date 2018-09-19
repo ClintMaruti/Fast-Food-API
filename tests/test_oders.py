1c72dd41f690d8ff25dc28f2668b28b92bc2da97
Clint <cmaruti93@gmail.com>
2018-09-20 01:06:48 +0300

    [Chore #160639115] Tests for POST endpoints

--- tests/test_oders.py
+++ tests/test_oders.py
@@ -5,9 +5,46 @@ from app import create_app
 app = create_app(config_name="TESTING")
 client = app.test_client()
 
+order_less_name = {
+    "id": 1,
+    "price": 800,
+    "status": "Delivered"
+}
+
+order_less_price = {
+    "id": 1,
+    "name": "Kebab",
+    "status": "Delivered"
+}
+
+nill_order = {}
+
+
 def test_all_orders():
     """
         Test for GET all orders
     """
     res = client.get('api/v1/orders')
     assert res.status_code == 200
+
+
+def test_nill_order(): 
+    """
+        Test empty order
+    """
+    res = client.post('api/v1/orders', data=nill_order)
+    assert res.status_code == 400
+
+def test_order_without_name():
+    """
+        Test order without name
+    """
+    res = client.post('api/v1/orders', data = order_less_name)
+    assert res.status_code == 400
+
+def test_order_without_price():
+    """
+        Test order without price
+    """
+    res = client.post('api/v1/orders', data= order_less_price)
+    assert res.status_code == 400
