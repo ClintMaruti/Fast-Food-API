05b3f848871dd81217609fa19aab9744577c2eb6
Clint <cmaruti93@gmail.com>
2018-09-26 16:57:03 +0300

    [Chore #160721014] restructure posts

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ orders = []
 class Orders(object):
   """ Class that Holds methods for the endpoints """
   def __init__(self, name=None, price=None, quantity=None):
-    """ We initialize an empty order list """
+
     self.id = len(orders) + 1
     self.name = name
     self.price = price
@@ -15,9 +15,7 @@ class Orders(object):
   def all_order(self):
     """ Model function that Fetchs all orders """
     if orders == []:
-      return ({"Message": "No Food Available.",
-                "Bucket:": orders})
-    
+      return orders
 
   def insert_order(self, name, price, quantity):
     """ Model function to Insert a new Order int list """
@@ -28,16 +26,14 @@ class Orders(object):
     self.bucketlist['quantity'] = quantity
     self.bucketlist['order_id'] = self.id
     orders.append(self.bucketlist)
-    return jsonify({"message": 'Your Order was placed successfully! ',
-                    "Bucket: ":  orders})
+    return orders[-1]
   
   def order_id(self, order_id):
     """ Model function to GET a specific order list """
     if isinstance(order_id, int):
       for order in orders:
         if order['order_id'] == id:
-          return jsonify({"message": "Success.", "Bucket": order})
-        return {'message': 'Order Not available'}
+          return order 
   
   def order_update(self, order_id,name,price,quantity):
     """ Model function to Update a specific order list """
@@ -47,9 +43,8 @@ class Orders(object):
           order['name'] == name
           order['price'] == price
           order['quantity'] == quantity
-          return jsonify({"message ": "Successful Update.",
-          "Bucket: ": orders}), 201
-        return ({"Message": "Update not succesfull.", "Bucket, ": orders})
+          return orders
+
 
 
 
--- app/resources/order_resources.py
+++ app/resources/order_resources.py
@@ -21,18 +21,35 @@ class OrderResources(Resource):
         endpoint to Get a list of orders
         """
         bucketall = bucket1.all_order()
-        return bucketall
-        
+        return jsonify ({"Message": "Success",
+                "Bucket:": bucketall}, 200)
+            
     def post(self):
         """
             endpoint to Insert new list of order
         """
+        parser = reqparse.RequestParser()
+        
         data = request.get_json()
+
+        parser.add_argument("name",type=str,required=True, help="Name cannot be blank!")
+        parser.add_argument("price",type=int,required=True, help='Rate cannot be converted')
+        parser.add_argument("quantity",type=str,required=True)
+
         name = data['name']
         price = data['price']
         quantity = data['quantity']
-        res = bucket1.insert_order(name,price,quantity)
-        return res
+        
+        if name == "":
+            return jsonify({"message": 'Order Invalid'}, 400)
+        elif price < 0:
+            return jsonify({"message": 'Order Invalid'}, 400)
+        elif quantity == "":
+            return jsonify({"message": 'Order Invalid'}, 400)
+        else:
+            res = bucket1.insert_order(name,price,quantity)
+        return jsonify({"message": 'Your Order was placed successfully! ',
+                    "Bucket: ":  res}, 200)
 
 class SpecificOrder(Resource):
     def get(self, order_id):
@@ -42,8 +59,7 @@ class SpecificOrder(Resource):
         if isinstance(order_id, int):
             bucket = bucket1.all_order()
             return bucket
-        return jsonify({"Message":"Invalid input"})
-
+        
     def put(self, order_id):
         """
         endpoint to Update a list of a specific order
@@ -52,17 +68,14 @@ class SpecificOrder(Resource):
 
         parser.add_argument("name",type=str,required=True)
         parser.add_argument("price",type=str,required=True)
-        parser.add_argument("quan0tity",type=str,required=True)
+        parser.add_argument("quantity",type=str,required=True)
         data = parser.parse_args()
         name = data['name']
         price = data['name']
         quantity = data['quantity']
 
-        order = {
-		'name':data['name'] 
-		}
 
         bucketupdate = bucket1.order_update(order_id,name,price,quantity)
         if isinstance(order_id, int):
             return bucketupdate
-        return jsonify({"message": "Error in the order update"})
+        return jsonify({"message": "Error in the order update"}, 404)
