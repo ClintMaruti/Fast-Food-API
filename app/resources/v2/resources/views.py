93647e9db1f8c56f67b5871d9e8759c60a156bcc
Clint <cmaruti93@gmail.com>
2018-09-29 16:06:06 +0300

    [feature #160870230] Adds food option to menu

--- app/resources/v2/resources/views.py
+++ app/resources/v2/resources/views.py
@@ -19,13 +19,49 @@ class Views(Resource):
 class OrderResources(Resource):
     def get(self):
         """
-            Endpoint to Get a list of orders
+            Endpoint to Get menu
         """
         if menu.all_order() is None:
             response = jsonify({"Message: ": "No order available!"})
             response.status_code = 400
             return response
         return menu.all_order()
+    
+    def post(self):
+        """
+            Endpoint to add food option to menu
+        """
+        parser = reqparse.RequestParser()
 
+        data = request.get_json()
+
+        parser.add_argument("name",type=str,required=True, help="Name cannot be blank!")
+        parser.add_argument("price",type=int,required=True, help='Rate cannot be converted')
+        parser.add_argument("quantity",type=int,required=True)
+
+        name = data['name']
+        price = data['price']
+        quantity = data['quantity']
+
+        if name == "":
+            response = jsonify({"Message": 'Name required. Invalid Order!'})
+            response.status_code = 400
+            return response
+        elif price <= 0:
+            response = jsonify({"Message": "Price must be greater than zero!"})
+            response.status_code = 400
+            return response
+        elif quantity <= 0:
+            response = jsonify({"Message": "Quantity cannot be less than zero!"})
+            response.status_code = 400
+            return response
+        else:
+            response = jsonify({"Message": "Your Order was placed successfully!"})
+            response.status_code = 201
+            return jsonify({"Your Order: ": menu.insert_order, "Message": response})
+    
+
+        
+    
 
 
