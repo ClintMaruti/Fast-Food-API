403be02c96aaec097d2c3923ba72426dda2aff58
Clint <cmaruti93@gmail.com>
2018-10-05 08:54:56 +0300

    j

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ def connect():
 
         print("Connecting to the PostgreSQL database...")
         # DATABASE = os.getenv('DATABASE_URL')
-        conn = psycopg2.connect("dbname=alpha user=postgres password=admin234")
+        conn = psycopg2.connect("dbname=test user=postgres password=admin234")
 
         #create cursor
         cur = conn.cursor()
